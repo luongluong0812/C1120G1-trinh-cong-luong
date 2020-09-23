dba3e22ebc5efd2d850c7be67529651a7b6e6cd4
Your Name <you@example.com>
2020-09-23 10:56:53 +0700

    mm

---
+++ modelu1/JS/thực hành/thuchanh.js
@@ -0,0 +1,9 @@
+ function dienTich(){
+    let inputWidth,area;
+    let inputHeight;
+    inputWidth = prompt("Enter the width");
+    inputHeight = prompt("Enter the height");
+
+    area = parseInt(inputWidth*inputHeight);
+    alert(area);
+ }
